87e7d3e8782facd6d9514aa33c330d2cd2e70ef9
smaitfulldaytgrs-hue <smaitfulldaytgrs@gmail.com>
2026-08-24 08:23:22 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // yang ditampilkan selalu yang paling baru dari spreadsheet.
 // ============================================================
 
-var CACHE_NAME = 'besic-store-shell-v3';
+var CACHE_NAME = 'besic-store-shell-v4';
 var PRECACHE_URLS = [
   './',
   './index.html',
